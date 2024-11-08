3bdaa0cee0dcc5e6b2781cdd52b2ccb569e0726f
Renato "Lond" Cerqueira <renato@lond.com.br>
2024-11-08 16:14:04 +0100

    Add terms of service

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  layout "home"
+  layout "home", only: %i[index]
+  layout "no_columns", only: %i[terms]
   allow_unauthenticated_access
 
   def index; end
+
+  def terms; end
 end
