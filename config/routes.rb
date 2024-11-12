7fd690ce4734955e1f534ed80c21dc707ae7b111
Renato "Lond" Cerqueira <renato@lond.com.br>
2024-11-12 12:26:22 +0100

    Link to terms and privacy policy from footer

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "terms" => "home#terms"
+  get "privacy" => "home#privacy"
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
