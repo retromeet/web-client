805b3fa2fa78ec18976d127b5e5f7a605453d827
Renato "Lond" Cerqueira <renato@lond.com.br>
2024-11-12 12:27:44 +0100

    Fix layout for privacy policy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,6 @@
 
 class HomeController < ApplicationController
   layout "home", only: %i[index]
-  layout "no_columns", only: %i[terms]
+  layout "no_columns", except: %i[index]
   allow_unauthenticated_access
-
-  def index; end
-
-  def terms; end
 end
