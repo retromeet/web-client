998c8b341ba022ff970b80e92bd74ece0cffcfee
Renato "Lond" Cerqueira <renato@lond.com.br>
2024-12-04 12:38:03 +0100

    Rubocop fix

--- app/controllers/image_proxy_controller.rb
+++ app/controllers/image_proxy_controller.rb
@@ -3,6 +3,6 @@
 class ImageProxyController < ApplicationController
   def image
     status, headers, body = retro_meet_client.image(path: request.path)
-    render body: body, content_type: headers["content-type"], status: status
+    render body:, content_type: headers["content-type"], status:
   end
 end
