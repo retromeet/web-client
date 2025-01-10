450cd3e9038dff7f0788b25a3cf1d3d53ee028de
Renato "Lond" Cerqueira <renato@lond.com.br>
2025-01-10 11:59:16 +0100

    Make system testing headless

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,12 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  headless = ENV.fetch("HEADLESS", true) != "false"
+
+  driven_by(:selenium, using: :chrome, screen_size: [1400, 1400]) do |driver|
+    driver.add_argument("--headless") if headless
+    driver.add_argument("--no-sandbox")
+    driver.add_argument("--disable-dev-shm-usage")
+    driver.add_argument("--disable-gpu")
+  end
 end
