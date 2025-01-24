c8752e9f754fc1c1d2863b49a6ad61737f5cda1b
Renato "Lond" Cerqueira <renato@lond.com.br>
2025-01-24 18:01:49 +0100

    Fix user agent
    Remove hard-coded http from user agent and use rails config for it

---
+++ config/initializers/retromeet.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+port = ENV.fetch("PORT", 3000)
+host = ENV.fetch("LOCAL_DOMAIN") { "localhost:#{port}" }
+
+https = Rails.env.production? || ENV["LOCAL_HTTPS"] == "true"
+
+Rails.application.configure do
+  config.x.use_https = https
+  config.x.retromeet_web_host = host
+end
