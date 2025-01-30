b4bef637611dd3f2dc7e3baf50541903e1a7de05
Renato "Lond" Cerqueira <renato@lond.com.br>
2025-01-30 09:21:24 +0100

    Close modal on outside click

--- app/javascript/controllers/bulma_modal_controller.js
+++ app/javascript/controllers/bulma_modal_controller.js
@@ -1,9 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
+import { useClickOutside } from 'stimulus-use'
 
 // Connects to data-controller="bulma-modal"
 export default class extends Controller {
-  static targets = ["root"]
-  close() {
+  static targets = ["root", "content"]
+  connect() {
+    useClickOutside(this, { element: this.contentTarget })
+  }
+  modalClose() {
     this.rootTarget.classList.remove("is-active")
+  }
+  close() {
+    this.modalClose()
+  }
+  clickOutside(event) {
+    this.modalClose()
   }
 }
