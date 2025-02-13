dec2aed35d7bcfdcfa6c1c531d0c0091d8f9d252
Renato "Lond" Cerqueira <renato@lond.com.br>
2025-02-13 23:11:58 +0100

    Warn if missing oauth

--- config/initializers/oauth2.rb
+++ config/initializers/oauth2.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
-Rails.application.configure do
-  config.x.oauth_client_id = ENV.fetch("OAUTH_CLIENT_ID")
-  config.x.oauth_client_secret = ENV.fetch("OAUTH_CLIENT_SECRET")
-  config.x.oauth_registration_access_token = ENV.fetch("OAUTH_REGISTRATION_ACCESS_TOKEN")
+if ENV["OAUTH_CLIENT_ID"]
+  Rails.application.configure do
+    config.x.oauth_client_id = ENV.fetch("OAUTH_CLIENT_ID")
+    config.x.oauth_client_secret = ENV.fetch("OAUTH_CLIENT_SECRET")
+    config.x.oauth_registration_access_token = ENV.fetch("OAUTH_REGISTRATION_ACCESS_TOKEN")
+  end
+else
+  Rails.logger.warn { "No OAUTH config was found. It is fine if you're generating it, but otherwise things will not work!" }
 end
